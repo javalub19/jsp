14d4d930691ed0f82a2653188c01e89120160c8b
lkre <lukasz.res@gmail.com>
2020-03-07 11:55:17 +0100

    zadanie 27

--- src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
@@ -1,5 +1,7 @@
 package pl.sda.jsp.servlet;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,10 +17,12 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     public void doGet(
             HttpServletRequest request
-            , HttpServletResponse response) throws IOException {
+            , HttpServletResponse response) throws IOException, ServletException {
         final PrintWriter writer = response.getWriter();
         writer.println("Hello world");
 
-        response.sendRedirect("home.jsp");
+//        response.sendRedirect("home.jsp");
+        final RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+        dispatcher.forward(request, response);
     }
 }
