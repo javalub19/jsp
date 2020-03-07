1c0195f06e8c3d1b47dd1f84c73ceb8e7bf754ce
lkre <lukasz.res@gmail.com>
2020-03-07 11:28:53 +0100

    zadanie 24

--- src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
@@ -1,7 +1,5 @@
 package pl.sda.jsp.servlet;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,21 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet"
-        , urlPatterns = {"/", "/hello"}
-        , initParams = {
-        @WebInitParam(name = "userId", value = "123456")
-        , @WebInitParam(name = "localization", value = "Poland")
-})
+//@WebServlet(name = "HelloWorldServlet"
+//        , urlPatterns = {"/", "/hello"}
+//)
 public class HelloWorldServlet extends HttpServlet {
-
-    private String userId, localization;
-
-    @Override
-    public void init(ServletConfig config) {
-        userId = config.getInitParameter("userId");
-        localization = config.getInitParameter("localization");
-    }
 
     @Override
     public void doGet(
@@ -31,7 +18,5 @@ public class HelloWorldServlet extends HttpServlet {
             , HttpServletResponse response) throws IOException {
         final PrintWriter writer = response.getWriter();
         writer.println("Hello world");
-        writer.println("userId: " + userId);
-        writer.println("localization" + localization);
     }
 }
